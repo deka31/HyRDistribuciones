f90f27ebd08dc648684132ecda06257eb50e868c
deka31 <de-ka31@hotmail.com>
2017-02-01 22:57:00 -0500

    Initial commit

---
+++ app/src/main/java/me/joserafael/hyrdistribuciones/ProcessInformation.java
@@ -0,0 +1,13 @@
+package me.joserafael.hyrdistribuciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProcessInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_information);
+    }
+}
